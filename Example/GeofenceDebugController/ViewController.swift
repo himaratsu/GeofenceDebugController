b5dd3508baf8bd7fd056c41574e048b158c419b6
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2016-09-20 22:02:31 +0900

    Updated: Geofence sample

--- Example/GeofenceDebugController/ViewController.swift
+++ Example/GeofenceDebugController/ViewController.swift
@@ -24,22 +24,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    @IBAction private func addGeofenceButtonTouched(sender: AnyObject) {
-        // Tokyo Tower
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+}
+
+// MARK: - IBAction
+extension ViewController {
+    @IBAction private func tokyoTowerButtonTouched(sender: AnyObject) {
         let center = CLLocationCoordinate2DMake(35.681382, 139.766084)
         let region = CLCircularRegion(center: center, radius: 200, identifier: "Tokyo Tower")
         locationManager.startMonitoringForRegion(region)
-        
-        showAlert("New geofence added")
+        showAlert("New geofence added", message: "Tokyo Tower")
     }
+    
+    @IBAction private func roppongiHillsTouched(sender: AnyObject) {
+        let center = CLLocationCoordinate2DMake(35.660428, 139.729182)
+        let region = CLCircularRegion(center: center, radius: 200, identifier: "Roppongi Hills")
+        locationManager.startMonitoringForRegion(region)
+        
+        showAlert("New geofence added", message: "Roppongi Hills")
+    }
+    
+    @IBAction private func asakusaButtonTouched(sender: AnyObject) {
+        let center = CLLocationCoordinate2DMake(35.711877, 139.796697)
+        let region = CLCircularRegion(center: center, radius: 200, identifier: "Asakusa")
+        locationManager.startMonitoringForRegion(region)
+        
+        showAlert("New geofence added", message: "Asakusa")
+    }
+    
     
     @IBAction private func debugButtonTouched(sender: AnyObject) {
         GeofenceDebugController.show(self)
-    }
-    
-    private func showAlert(message: String) {
-        let alertController = UIAlertController(title: "Success", message: message, preferredStyle: .Alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-        presentViewController(alertController, animated: true, completion: nil)
     }
 }
